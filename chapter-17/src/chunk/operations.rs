85b6ad9af875a9d2dc502312f86457f705b14971
pyromechanical <andreaogara@gmail.com>
2023-08-19 23:24:11 -0400

    added archive of chapter 17

---
+++ chapter-17/src/chunk/operations.rs
@@ -0,0 +1,40 @@
+#[repr(u8)]
+#[derive(Clone, Copy, Debug)]
+pub enum OpCode {
+    Constant = 0,
+    Negate,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Return
+}
+impl TryInto<OpCode> for u8 {
+    type Error = ();
+    fn try_into(self) -> Result<OpCode, Self::Error> {
+        if self > OpCode::Return as u8 {
+            Err(())
+        } else {
+            Ok(unsafe{std::mem::transmute(self)})
+        }
+    }
+}
+
+impl Into<u8> for OpCode {
+    fn into(self) -> u8 {
+        unsafe{std::mem::transmute(self)}
+    }
+}
+
+//must be repr(u8) to avoid undefined behavior!
+#[repr(u8)]
+#[derive(Clone, Copy, Debug)]
+pub enum Operation {
+    Constant{index: u8} = 0,
+    Negate,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Return
+}
